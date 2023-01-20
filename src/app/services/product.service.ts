ccf0c674c6c71e50b1fdd683d4627385f6f7e798
Kemal AKBAY <kemalakbay10@gmail.com>
2023-01-20 23:13:47 +0300

    20th lesson end

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/ResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,8 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
     let newPath:string=this.apiUrl+'Products/getbycategory?categoryId='+categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Products/add",product);
   }
 }
